9ef2e314ca03419467be8bbcfd61e6d4f1053837
Zlotnikov Andrey <zlotnikov171@gmail.com>
2022-03-07 18:02:44 +0300

    final test completed

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,21 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client:{name, phone},order:{address, sum}, goods[{title, count}]};
+    let data = {goods: [], order: {}};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title[i]);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({"title":goods[i].title,"count":goods[i].count});
     }
 
-
-    data.order.address = address;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat ;
     data.order.sum = sum;
-    data.client.name = name;
-    data.client.phone = phone;
 
-    data.client = 'Иван';
+    data.client = name + " " + phone;
 
     let jsonData = JSON.stringify({data});
 
